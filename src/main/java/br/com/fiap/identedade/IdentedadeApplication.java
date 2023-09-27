0b4d0812792bd0f7145396b3ea10df40f888805a
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2023-09-26 21:07:39 -0300

    update nas properties

--- src/main/java/br/com/fiap/identedade/IdentedadeApplication.java
+++ src/main/java/br/com/fiap/identedade/IdentedadeApplication.java
@@ -3,6 +3,7 @@ package br.com.fiap.identedade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//testando
 @SpringBootApplication
 public class IdentedadeApplication {
 
